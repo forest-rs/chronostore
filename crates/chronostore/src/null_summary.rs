9340ec25f3ba69db9c91fe4fd9a0415a5f8fcbf4
Bruce Mitchener <bruce.mitchener@gmail.com>
2026-06-14 14:52:34 +0700

    Adopt shared repository hygiene files

--- crates/chronostore/src/null_summary.rs
+++ crates/chronostore/src/null_summary.rs
@@ -1,8 +1,5 @@
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// Copyright 2026 the Chronostore Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use crate::{Entry, Summary};
 use core::marker::PhantomData;
